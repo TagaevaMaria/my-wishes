f80a08f781000247ac199f38585ca4313d4c0982
TagaevaMaria <vcebudet5.okyy@gmail.com>
2022-10-23 23:27:41 +0700

    [add] dev экран фото, проверка.

--- lib/ui/widget/menu/foto/add_foto_widget.dart
+++ lib/ui/widget/menu/foto/add_foto_widget.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 class AddFotoWidgetModel {
   final String foto;
@@ -9,6 +10,22 @@ class AddFotoWidgetModel {
 class AddFotoWidget extends StatelessWidget {
   const AddFotoWidget({Key? key}) : super(key: key);
 
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: FotoList(),
+    );
+  }
+}
+
+class FotoList extends StatefulWidget {
+  FotoList({Key? key}) : super(key: key);
+
+  @override
+  _FotoListState createState() => _FotoListState();
+}
+
+class _FotoListState extends State<FotoList> {
   @override
   Widget build(BuildContext context) {
     return Container();
